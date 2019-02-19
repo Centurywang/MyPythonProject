8bf71c4566e753e2fd5673e656ab7eeb61dbeb4f
master <1649601714@qq.com>
2019-02-19 11:43:51 +0800

    添加妹子图爬虫的描述信息

--- GetMeizitu/README.md
+++ GetMeizitu/README.md
@@ -4,5 +4,3 @@
     GitMeizitu.py 爬取套图标题以及url并保存至mysql数据库
     get_data_from_mysql.py 调用download_picture.py文件 从mysql数据库获取数据并下载至本地
    
-      
-    
--- GetMeizitu/get_data_from_mysql.py
+++ GetMeizitu/get_data_from_mysql.py
@@ -5,6 +5,7 @@ mysql = MySQLDB()
 sql = 'select * from meizitu'
 data = mysql.execute_sql(sql)
 
+# 在此设置要爬取的套图范围
 for i in data[15:30]:
     print(i)
     download_pictures(i)
